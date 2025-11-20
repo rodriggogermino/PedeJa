ae059c96fc7e599e8af016740f6ac16e4c8a53bc
Rodrigo Germino <rodrigoggermino@gmail.com>
2025-11-20 16:08:25 +0000

    Dashboard (CSS ligeiramente diferente do Mockup)

--- README.md
+++
@@ -1,2 +0,0 @@
-# PedeJa
-PedeJá - Com o PedeJá fazes o teu pedido no bar da escola em segundos. Escolhe, agenda e levanta sem filas!
---
+++ js/script.js
@@ -0,0 +1,12 @@
+function entrar() {
+    let email = document.getElementById("email").value
+    let pass = document.getElementById("password").value
+
+    if(email == "admin@pedeja.com" && pass == "admin123")  {
+        window.location.href = 'dashboardAdmin.html';
+    } else if(email == "aluno@pedeja.com" && pass == "aluno123") {
+        window.location.href = 'dashboardAluno.html';
+    } else {
+        alert("Dados incorretos!")
+    }
+}
