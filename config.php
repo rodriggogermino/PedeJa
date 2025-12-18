596cb609742c5af93c745e260615d7485778f8d5
Rodrigo Germino <rodrigoggermino@gmail.com>
2025-12-18 12:05:16 +0000

    Teste Hostinger BD

--- config.php
+++ config.php
@@ -1,7 +1,21 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "pedeja");
+if ($_SERVER['SERVER_NAME'] == 'localhost' || $_SERVER['SERVER_NAME'] == '127.0.0.1') {
+    $host = "localhost";
+    $user = "root";
+    $pass = "";
+    $dbName = "pedeja"; 
+} else {
+    $host = "localhost";
+    $user = "u506280443_rodguidbUser";
+    $pass = "5f1WXa:T+";
+    $dbName = "u506280443_rodguiDB";
+}
+
+$conn = new mysqli($host, $user, $pass, $dbName);
 
 if ($conn->connect_error) {
     die("Erro na ligação: " . $conn->connect_error);
 }
+
+$conn->set_charset("utf8mb4");
 ?>
